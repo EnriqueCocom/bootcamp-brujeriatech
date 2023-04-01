2600b92ee5732cdbef7582d9be10a55aa78d7a6b
EnriqueCocom <LE18080485@merida.tecnm.mx>
2023-03-31 23:56:35 -0600

    Agregando codigo de python

---
+++ ejercicios_python/holamundo.py
@@ -0,0 +1,17 @@
+# Esto es un comentario ve más rápido.
+
+"""
+ESTO ES UN COMENTARIO MUNTILINEA
+PUEDO PONER VARIAS LINEAS DE COMENTARIO
+"""
+# HOLA MUNDO EN PYTHON
+
+#Esto es una variable, definirla antes ***
+saludo = "Hola mundo!!!!!"
+print(saludo)
+
+saludo = "Adios"
+print(saludo)
+
+saludo = 5
+print(saludo)
---
+++ ejercicios_python/tipo_de_datos.py
@@ -0,0 +1,46 @@
+# Tipos de datos
+
+dato = "5" # Una cadena de caracteres o string o cadena de texto
+print(dato + dato)
+
+dato = 5 # númerico - Entero sin decimales
+print(dato + dato)
+
+dato = 5.0 # númerico - Flotante - Con punto decimal
+print(dato + dato)
+
+dato = False # Booleano - Verdadero o Falso - True o False
+print(dato + dato)
+
+# La función exit cierra el programa de python
+print("💖")
+print("你好世界")
+exit()
+
+
+cinco = "5"
+numero = 5
+
+#Solución 1 : convertir el valor
+
+print(float(cinco) +  numero)
+
+#Solución 2: Formato de cadenas
+
+nombre = "Enrique"
+edad = 24
+
+# El objetivo es imprimir: Hola, mi nombre es Enrique y tengo 24 años
+# String es una clase que tiene objetos
+
+print("Hola, mi nombre es {} y tengo {} años".format(nombre, edad))
+
+#Solucion 3: f-string
+
+print(f"Hola, mi nombre es {nombre} y tengo {edad} años")
+
+#Solución 1 de nuevo: Cast del dato con concatenacion
+
+print("Hola, mi nombre es " + nombre + " y tengo " + str(edad) + " años")
+
+print(_name_)
